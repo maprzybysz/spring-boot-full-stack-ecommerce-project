ff4954547191f9a8f5a636dd8665d1f532ff3ec8
Mateusz Przybysz <mprzybysz.dev@gmail.com>
2026-04-26 10:53:38 +0200

    Setting up CategoryService

--- sb-ecom/src/main/java/com/ecommerce/sbecom/controller/CategoryController.java
+++ sb-ecom/src/main/java/com/ecommerce/sbecom/controller/CategoryController.java
@@ -1,28 +1,31 @@
 package com.ecommerce.sbecom.controller;
 
 import com.ecommerce.sbecom.model.Category;
+import com.ecommerce.sbecom.service.CategoryService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class CategoryController {
 
-    private List<Category> categories = new ArrayList<>();
+    private final CategoryService categoryService;
 
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
 
     @GetMapping("api/public/categories")
     public List<Category> getAllCategories() {
-        return categories;
+        return categoryService.getAllCategories();
     }
 
     @PostMapping("/api/public/categories")
     public String createCategory(@RequestBody Category category) {
-        categories.add(category);
+        categoryService.createCategory(category);
         return "Category added successfully";
     }
 }
