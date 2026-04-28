fad78db52d68d0c192304a3c4b2d646db7fe60e9
Mateusz Przybysz <mprzybysz.dev@gmail.com>
2026-04-28 20:42:54 +0200

    Defining JPA repositories

---
+++ sb-ecom/src/main/java/com/ecommerce/sbecom/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.ecommerce.sbecom.repository;
+
+import com.ecommerce.sbecom.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
