f08e99c9f7389728f7a3cfe0188d23dfb7af98bd
Mateusz Przybysz <mprzybysz.dev@gmail.com>
2026-05-01 13:35:03 +0200

    Implement ResourceNotFoundException in services

--- sb-ecom/src/main/java/com/ecommerce/sbecom/service/CategoryServiceImpl.java
+++ sb-ecom/src/main/java/com/ecommerce/sbecom/service/CategoryServiceImpl.java
@@ -1,10 +1,9 @@
 package com.ecommerce.sbecom.service;
 
+import com.ecommerce.sbecom.exceptions.ResourceNotFoundException;
 import com.ecommerce.sbecom.model.Category;
 import com.ecommerce.sbecom.repository.CategoryRepository;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -30,7 +29,7 @@ public class CategoryServiceImpl implements CategoryService {
     @Override
     public String deleteCategory(Long categoryId) {
         Category category = categoryRepository.findById(categoryId)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found"));
+                .orElseThrow(() -> new ResourceNotFoundException("Category", "categoryId", categoryId));
 
         categoryRepository.delete(category);
         return "Category with categoryId: " + categoryId + " deleted successfully!";
@@ -39,7 +38,7 @@ public class CategoryServiceImpl implements CategoryService {
     @Override
     public Category updateCategory(Long categoryId, Category category) {
         Category savedCategory = categoryRepository.findById(categoryId)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found"));
+                .orElseThrow(() -> new ResourceNotFoundException("Category", "categoryId", categoryId));
         savedCategory.setCategoryName(category.getCategoryName());
         return categoryRepository.save(savedCategory);
     }
