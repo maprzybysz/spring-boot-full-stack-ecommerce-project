02d738290d35d919e9ac6c25ddbffd6bcaac92ac
Mateusz Przybysz <mprzybysz.dev@gmail.com>
2026-05-01 13:54:19 +0200

    Validation for no categories present

--- sb-ecom/src/main/java/com/ecommerce/sbecom/service/CategoryServiceImpl.java
+++ sb-ecom/src/main/java/com/ecommerce/sbecom/service/CategoryServiceImpl.java
@@ -19,7 +19,11 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public List<Category> getAllCategories() {
-        return categoryRepository.findAll();
+        List<Category> categories = categoryRepository.findAll();
+        if (categories.isEmpty()) {
+            throw new APIException("No categories created till now!");
+        }
+        return categories;
     }
 
     @Override
